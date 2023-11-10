74226b43f6172f6a9992255d740614d3f20acaf0
aaalaaeddine <alaedaoudi2003@gmail.com>
2023-11-10 09:21:10 -0500

    ajout formulaire sur login

--- pages/login.php
+++ pages/login.php
@@ -1,2 +1,16 @@
 <h2>connexion</h2>
 <a href="../">Accueil</a>
+<form action="../results/loginResult.php" method="post">
+    <label for="user_name">nom d'utilisateur</label>
+    <input id="user_name" type="text" name="user_name" value="">
+    
+    <label for="pwd">mot de passe</label>
+    <input id="pwd" type="password" name="pwd" value="">
+     
+    <button type="submit">Se connecter </button>
+
+
+
+
+
+</form>
