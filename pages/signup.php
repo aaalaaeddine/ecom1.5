404f6d9865800255921871a9364920a663b679fd
aaalaaeddine <alaedaoudi2003@gmail.com>
2023-11-10 09:21:52 -0500

    ajout formulaire sur signup

--- pages/signup.php
+++ pages/signup.php
@@ -1,2 +1,18 @@
 <h2>enregitrement</h2>
 <a href="../">Accueil</a>
+<form action="../results/signupResult.php" method="post">
+    <label for="user_name">nom d'utilisateur</label>
+    <input id="user_name" type="text" name="user_name" value="">
+    <label for="email">Email</label>
+    <input id="email" type="email" name="email" value="">
+    <label for="pwd">mot de passe</label>
+    <input id="pwd" type="password" name="pwd" value="">
+     
+    <button type="submit">enregistrer</button>
+
+
+
+
+
+</form>
+
